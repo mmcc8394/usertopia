eb7a5c4a33aa05dd3032ad2e163eb519702f631e
Matt McCormick <mmcc@jcdrepair.com>
2019-11-18 20:06:11 -0600

    Adding useful links to user class.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,17 @@
 class User < ApplicationRecord
+  #
+  # Multiple roles per user is based on this post & this gem.
+  #   Post: http://dmitrypol.github.io/2016/09/29/roles-permissions.html
+  #   Gem: https://github.com/brainspec/enumerize
+  #
   extend Enumerize
+  enumerize :roles, in: [ :admin, :basic, :auditor ], multiple: true, predicates: true
+  validates :roles, presence: true
 
   has_secure_password
 
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }, unless: :skip_password_validation?
-  validates :roles, presence: true
-
-  enumerize :roles, in: [ :admin, :basic, :auditor ], multiple: true, predicates: true
 
   def update_non_password_attributes(params)
     @skip_password_validation = true
