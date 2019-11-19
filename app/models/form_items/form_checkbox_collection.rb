d248180656b8ed918e9a9d64e19df32b2263ea93
Matt McCormick <mmcc@jcdrepair.com>
2019-11-18 20:42:21 -0600

    Getting the checkbox buttons to style right.

--- app/models/form_items/form_checkbox_collection.rb
+++ app/models/form_items/form_checkbox_collection.rb
@@ -2,6 +2,8 @@ class FormCheckboxCollection < FormItem
   private
 
   def form_item(field, options)
-    @form.collection_check_boxes(field, options[:collection], options[:value_function], options[:label_function])
+    '<span class="option">' +
+      @form.collection_check_boxes(field, options[:collection], options[:value_function], options[:label_function]) +
+      '</span>'
   end
 end
