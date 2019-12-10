63026e09d7385f77871e95e9c453720b8cfaecb3
Matt McCormick <mmcc@jcdrepair.com>
2019-12-10 10:32:00 -0600

    Better code to prevent non-admin updating roles.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,15 +36,11 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
 
-    #
-    # Need to set a user's new role(s) before calling authorize or the policy will always pass
-    # because if the current_user is the same as @user at this point, then the user_policy function
-    # updating_roles? will always pass.
-    #
-    set_roles_if_submitted
+    # Must do this before authorize or UserPolicy:updating_roles? always return true.
+    @user.assign_attributes(user_params_update)
     authorize(@user)
 
-    if @user.update(user_params_update)
+    if @user.save
       redirect_to user_path(@user), notice: 'User info updated.'
     else
       flash[:alert] = @user.errors.full_messages.join('<br />')
@@ -93,10 +89,5 @@ class UsersController < ApplicationController
 
   def user_params_password_update
     params.require(:user).permit(:password, :password_confirmation)
-  end
-
-  def set_roles_if_submitted
-    return if params[:user].try(:[], :roles).blank?
-    @user.roles = params[:user][:roles]
   end
 end
